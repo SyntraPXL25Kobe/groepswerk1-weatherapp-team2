466308f7305f265bf5790b616102c65f2d2ee588
SyntraPXL25Kobe <Kobe.Bervoets@cursist.syntrapxl.be>
2025-11-19 11:10:53 +0100

    Add initial variables

---
+++ app.js
@@ -0,0 +1,7 @@
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const iconUrl = "https://openweathermap.org/img/wn/";
+const apiKey = "38d41c5560fd9cdbd9cd0686d13a47b5";
+const defaultLocation = {
+  city: "Genk",
+  country: "BE",
+};
